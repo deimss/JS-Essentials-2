7569f1276c83344e0cece120efefdbd5b1604be9
deimss <deimss95@gmail.com>
2017-05-26 17:36:25 +0300

    add files for homework js2

---
+++ js/scripts.js
@@ -0,0 +1,112 @@
+ //Task 1
+function sumOne(num1,num2){
+  var sum = 0;
+  while (num1<=num2){
+    sum += num1;
+     num1++;
+  }
+console.log('Сума чисел від 1 до 3 = ' + sum);
+}
+sumOne(1,3);
+
+ //Task 2
+ function sumTwo(num1,num2){
+   var sum = 0;
+   while (num1<=num2){
+     sum += num1;
+      num1++;
+   }
+ console.log('Сума чисел від 20 до 220 = '+ sum);
+ }
+ sumTwo(20,220);
+
+//Task 3
+ function evenNum(num1,num2){
+   console.log( "Всі парні числа від 1-20 з continue " );
+   for(var i=num1 ; i<=num2 ; i++){
+     if (i%2 === 1) continue;
+     console.log(i +' ');
+ }
+}
+ evenNum(1,20);
+
+ //Task 4
+ function evenNum2(num1,num2){
+ console.log( "Всі парні числа від 1-20 " );
+  for(var i=num1 ; i<=num2 ; i++){
+    if (i%2 === 0){
+     console.log(i +' ');
+    }
+  }
+ }
+evenNum2(1,20);
+
+ //Task 5
+ function oddNum(num1,num2){
+  console.log( "Всі непарні числа від 10-30 " );
+   var i= num1;
+    while (i<=num2){
+     if (i%2 === 1){
+      console.log(i +' ');
+       }
+      i++;
+  }
+ }
+ oddNum(10,30);
+
+//Task 6
+ function birdNum(num1,num2){
+  console.log( "Числа від 1-5 ,з виходом коли день народження " );
+  var i = num1;
+  for (i=1; i<=num2;i++){
+    if (i == 6)break;
+    else{
+       console.log(i +' ');
+    }
+   }
+}
+birdNum(1,15);
+
+//Task 7
+function codeWhile(){
+  console.log('Рельтат використовючи while замість for');
+ var i = 0;
+  while (i < 5){
+  console.log('number '+i+'  =)');
+  i++;
+  }
+ }
+codeWhile();
+
+//Task 8
+function number100(){
+  console.log('Завдання з prompt');
+		var i = prompt("Type number bigger than 100!");
+		while (i < 101){
+			i = prompt("Type number bigger than 100!");
+		}
+    console.log('Number is ' +i);
+}
+number100();
+
+//Task 9
+function number100d0While(){
+    do{
+      i = prompt("Type number bigger than 100!");
+    }while (i < 101);
+    console.log('Number is ' + i);
+}
+number100d0While();
+
+//Task 10
+function showFor (){
+  var num = prompt('Type numbers of line of #');
+  var j= '#';
+ for (var i=0; i < num; i++){
+   console.log(j);
+   var j = j + '#';
+ }
+}
+showFor();
+
+//Task 7
